e8cf5075effe8e31e812112922a53557ca705364
p.rafalskij <rafalskypavel@gmail.com>
2023-06-02 12:43:14 +0300

    pnevmex: to parse using recursion. renamed.

--- pnevmex.py
+++ pnevmex.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 import time
+from requests.exceptions import ConnectTimeout
 
 URLS = "https://pnevmex.ru/brands/camozzi/"
 
@@ -9,13 +10,18 @@ cards = []
 
 
 def get_html(url):
-    """Получение HTML-кода страницы"""
-    headers = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0",
-    }
-    html = requests.get(url, headers=headers)
-    return html
+    """Функция для получения HTML-кода страницы с повторными попытками"""
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64)'}
+    while True:
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+            response.raise_for_status()  # Проверяем статус запроса
+            return response
+        except ConnectTimeout:
+            print(f'Превышено время ожидания соединения с: {url}')
+        except requests.exceptions.RequestException as e:
+            print(f'Ошибка при отправке запроса: {e}')
+        print(f'Повторная попытка парсинга страницы: {url}')
 
 
 def get_content(html):
@@ -50,46 +56,79 @@ def get_content(html):
     return cards
 
 
-def parser(url):
-    """Рекурсивная функция парсинга"""
-    print(f'Парсим данные с: "{url}"', url.title())
+def get_page_count(url):
+    """Функция для получения количества страниц в группе"""
     html = get_html(url)
     if html.status_code == 200:
-        print(f'Парсинг страницы: {1}')
+        soup = BeautifulSoup(html.text, 'html.parser')
+        pagination = soup.find('div', {'class': 'pagination'})
+        if pagination is not None:
+            pages = pagination.findAll('a')
+            last_page = pages[-1].text.strip() if pages else 1
+            return int(last_page)
+    return 1
+
+
+def parse_subgroup(url, visited_urls):
+    # Если URL уже посещен, выходим из функции
+    if url in visited_urls:
+        return
+
+    # Добавляем URL в список посещенных
+    visited_urls.add(url)
+
+    # Получаем HTML-код страницы
+    html = get_html(url)
+
+    # Если статус ответа 200 (успешный запрос)
+    if html.status_code == 200:
+        # Выводим информацию о парсинге текущей страницы
+        print(f'Парсинг страницы: {url}')
+
+        # Извлекаем данные со страницы
         get_content(html)
+
+        # Выводим общее количество товаров
         print(f'Всего: {len(cards)} позиций')
 
+        # Проверяем наличие пагинации на подстраницах
         soup = BeautifulSoup(html.text, 'html.parser')
+        pagination = soup.find('div', {'class': 'pagination'})
+        if pagination is not None:
+            pages = pagination.findAll('a')
+            # Перебираем каждую страницу пагинации
+            for page in pages:
+                # Формируем URL страницы
+                page_url = 'https://pnevmex.ru' + page['href']
+                # Исключаем повторный парсинг текущей страницы
+                if page_url != url:
+                    # Рекурсивно вызываем функцию parse_subgroup() для парсинга следующей страницы пагинации
+                    parse_subgroup(page_url, visited_urls.copy())
+
+        # Рекурсивно вызываем функцию parse_subgroup() для парсинга подгрупп
         itemsLevel = soup.findAll('div', {'class': 'type'})
         for item in itemsLevel:
             print("Опустились в подгруппу ", item.text.strip())
-            s = 'https://pnevmex.ru' + item.find('a', {'class': 'type-link'}).get('href')
-            print(s, " Ссылка на группу: ", item.text.strip())
-            parser(s)
+            subgroup_url = 'https://pnevmex.ru' + item.find('a', {'class': 'type-link'}).get('href')
+            print(subgroup_url, " Ссылка на группу: ", item.text.strip())
+            parse_subgroup(subgroup_url, visited_urls.copy())
 
     else:
-        print(f'Ответ сервера:{html.status_code}. Парсинг невозможен!')
+        print(f'Ответ сервера: {html.status_code}. Парсинг невозможен!')
+
+
+def parser(url):
+    """Функция парсинга"""
+    visited_urls = set()
+    parse_subgroup(url, visited_urls)
 
 
 if __name__ == "__main__":
-
-    # Начало замера времени
     start_time = time.time()
-
     parser(URLS)
-
-    # Конец замера времени
     end_time = time.time()
-
-    # Вычисление времени выполнения
     execution_time = end_time - start_time
-    # Вывод времени выполнения
     print(f"Время выполнения: {execution_time} секунд")
-
-
     dataframe = pandas.DataFrame(cards)
-    with pandas.ExcelWriter("pnevmex.xlsx",
-                            mode="a",
-                            engine="openpyxl",
-                            if_sheet_exists="overlay", ) as writer:
+    with pandas.ExcelWriter("pnevmex.xlsx", mode="a", engine="openpyxl", if_sheet_exists="overlay") as writer:
         dataframe.to_excel(writer, sheet_name='pnevmex')
